77c6dd4287bca3722d3d64c7fbdc7b938f3ff6f3
Wang Kan <44154535+shuiipkw@users.noreply.github.com>
2020-06-18 20:21:24 +0800

    Update test_reconstruction_real.m

--- CPU/Test_Reconstruction/test_reconstruction_real.m
+++ CPU/Test_Reconstruction/test_reconstruction_real.m
@@ -58,16 +58,16 @@ tic;
 iter=1;
 while iter <=10
     fprintf('Sample--%f---Round--#%d\n', 2, iter);
-    %[X_f_trans] = alter_min_LS_one_step(T_omega_f_trans, omega_f_trans * 1/k, Y_f_trans);
-    [X_f_trans] = alter_min_LS_one_step(T_omega_f_trans, omega_f_trans, Y_f_trans);
+    [X_f_trans] = alter_min_LS_one_step(T_omega_f_trans, omega_f_trans * 1/k, Y_f_trans);
+    %[X_f_trans] = alter_min_LS_one_step(T_omega_f_trans, omega_f_trans, Y_f_trans);
     
     for i =1:k
         X_f(:,:,i) = X_f_trans(:,:,i)';
     end
 
     % Given X, do LS to get Y
-    %[Y_f] = alter_min_LS_one_step(T_omega_f, omega_f * 1/k, X_f);
-    [Y_f] = alter_min_LS_one_step(T_omega_f, omega_f, X_f);
+    [Y_f] = alter_min_LS_one_step(T_omega_f, omega_f * 1/k, X_f);
+    %[Y_f] = alter_min_LS_one_step(T_omega_f, omega_f, X_f);
     
     for i = 1: k
         Y_f_trans(:,:,i) = Y_f(:,:,i)';
@@ -91,10 +91,10 @@ T_est1 = T_est1*mean(T1(:))/mean(T_est1(:));
 toc;
 temp = T1 - T_est1;   
 error = norm(temp(:)) / norm(T1(:));
-PSNR = [];
-for i=1:num_miss_frame
-    PSNR = [PSNR, psnr(T1(:, :, miss_index(i)), T_est1(:, :, miss_index(i)))];
-end
+%PSNR = [];
+%for i=1:num_miss_frame
+%    PSNR = [PSNR, psnr(T1(:, :, miss_index(i)), T_est1(:, :, miss_index(i)))];
+%end
 fprintf("error=%.4f\n", error);
 disp(['runtime:',num2str(toc)]);
 %for i=1:k
